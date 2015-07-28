e694162a4208688341c284aed6c679c603e09c38
Albert Tholen <albert.tholen@gmail.com>
2015-07-28 18:09:52 -0400

    Add latest blog post to index. Begin building nav bar.

--- app.rb
+++ app.rb
@@ -23,18 +23,6 @@ class Field < ActiveRecord::Base
   end
 end
 
-get "/" do
-  @fields_map = {
-    :about   => Field.where( section: "about" ).to_a,
-    :work    => Field.where( section: "work" ).to_a,
-    :gear    => Field.where( section: "gear" ).to_a,
-    :contact => Field.where( section: "contact" ).to_a
-  }
-
-  @title = "Welcome"
-  haml :"fields/index"
-end
-
 helpers do
   def title
     if @title
@@ -48,6 +36,61 @@ end
 helpers do
   include Rack::Utils
   alias_method :h, :escape_html
+end
+
+
+##########################################################
+######                   Tumblr                    #######
+##########################################################
+
+
+# Tumblr.configure do |config|
+#   config.consumer_key = "adPBCUStfJLM87SWP7a8DhkZoJMqMpUt8zuR9ohYs1TUKeF6oq"
+#   config.consumer_secret = "3MgmCEMK24dAByB4EUyyk91w1cjYXUfFqZrFAGipd6axiYckOq"
+# end
+
+client = Tumblr::Client.new({
+    :consumer_key => 'adPBCUStfJLM87SWP7a8DhkZoJMqMpUt8zuR9ohYs1TUKeF6oq',
+    :consumer_secret => '3MgmCEMK24dAByB4EUyyk91w1cjYXUfFqZrFAGipd6axiYckOq',
+    :oauth_token => 'ptmdNi2bW3QVemYEmARK03b4YFceBRnc8b8jOkXeNXEjo4PLMG',
+    :oauth_token_secret => 'ozyLNwhEUZruJb5CEHHYDq053fgmNzKUyHaGDLL0XPa9cnDrdn'
+  })
+
+# get_posts is a hash with keys "blog" (for general blog info), "posts" (an array of the posts), and "total_posts" (integer)
+
+# get_posts = client.posts(:hostname => "aaronmicahzick.tumblr.com", :api_key => "adPBCUStfJLM87SWP7a8DhkZoJMqMpUt8zuR9ohYs1TUKeF6oq", :limit => 10)
+get_posts = client.posts "aaronmicahzick.tumblr.com"
+
+#Initialize the posts fetch beforehand, pass the found posts into /blog
+# posts = []
+# total_posts = 0
+# get_posts.perform do |response|
+#     if response.success?
+#         parsed_response = response.parse
+#         total_posts = parsed_response["response"]["blog"]["posts"]
+#         puts parsed_response
+#         posts =  parsed_response["response"]["posts"]
+#     end
+# end
+
+
+##########################################################
+######                   ROUTING                   #######
+##########################################################
+
+
+get "/" do
+  @fields_map = {
+    :about   => Field.where( section: "about" ).to_a,
+    :work    => Field.where( section: "work" ).to_a,
+    :gear    => Field.where( section: "gear" ).to_a,
+    :contact => Field.where( section: "contact" ).to_a
+  }
+
+  @latest_post = get_posts["posts"][0]
+
+  @title = "Welcome"
+  haml :"fields/index"
 end
 
 post "/edit" do
@@ -79,41 +122,6 @@ get "/edit" do
   @title = "Edit Page"
   haml :"fields/edit"
 end
-
-# Tumblr.configure do |config|
-#   config.consumer_key = "adPBCUStfJLM87SWP7a8DhkZoJMqMpUt8zuR9ohYs1TUKeF6oq"
-#   config.consumer_secret = "3MgmCEMK24dAByB4EUyyk91w1cjYXUfFqZrFAGipd6axiYckOq"
-# end
-
-client = Tumblr::Client.new({
-    :consumer_key => 'adPBCUStfJLM87SWP7a8DhkZoJMqMpUt8zuR9ohYs1TUKeF6oq',
-    :consumer_secret => '3MgmCEMK24dAByB4EUyyk91w1cjYXUfFqZrFAGipd6axiYckOq',
-    :oauth_token => 'ptmdNi2bW3QVemYEmARK03b4YFceBRnc8b8jOkXeNXEjo4PLMG',
-    :oauth_token_secret => 'ozyLNwhEUZruJb5CEHHYDq053fgmNzKUyHaGDLL0XPa9cnDrdn'
-  })
-
-# get_posts = client.posts(:hostname => "aaronmicahzick.tumblr.com", :api_key => "adPBCUStfJLM87SWP7a8DhkZoJMqMpUt8zuR9ohYs1TUKeF6oq", :limit => 10)
-get_posts = client.posts "aaronmicahzick.tumblr.com"
-
-####
-#
-# get_posts is a hash with keys "blog" (for general blog info), "posts" (an array of the posts), and "total_posts" (integer)
-#
-####
-
-# puts get_posts
-
-#Initialize the posts fetch beforehand, pass the found posts into /blog
-# posts = []
-# total_posts = 0
-# get_posts.perform do |response|
-#     if response.success?
-#         parsed_response = response.parse
-#         total_posts = parsed_response["response"]["blog"]["posts"]
-#         puts parsed_response
-#         posts =  parsed_response["response"]["posts"]
-#     end
-# end
 
 get "/blog" do
     # @posts = posts
