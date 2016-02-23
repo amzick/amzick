f6b586f42e86205c84e4925bb2440b03b37f42fc
Albert Tholen <albert.tholen@gmail.com>
2016-02-23 12:05:46 -0500

    Admin credentials.

--- app.rb
+++ app.rb
@@ -29,7 +29,7 @@ helpers do
 
   def authorized?
     @auth ||=  Rack::Auth::Basic::Request.new(request.env)
-    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['admin', 'admin']
+    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['amgz232', 'godehtybrik232']
   end
 
   def title
